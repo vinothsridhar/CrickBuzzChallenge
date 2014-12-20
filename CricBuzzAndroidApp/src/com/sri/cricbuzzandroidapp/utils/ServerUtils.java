63a8ecf653589e111e9e11df6cb2a7766bdd2001
sridhar <sridhar@passportparking.com>
2014-12-20 22:59:10 +0530

    first commit

---
+++ CricBuzzAndroidApp/src/com/sri/cricbuzzandroidapp/utils/ServerUtils.java
@@ -0,0 +1,9 @@
+package com.sri.cricbuzzandroidapp.utils;
+
+
+public class ServerUtils {
+
+	public static final String PLAYLIST_URL="http://m.cricbuzz.com/interview/playerlist";
+	public static final String IMAGE_URL = "http://www.cricbuzz.com/includes/stats/img/";
+
+}
